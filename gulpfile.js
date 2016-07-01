dd527cb19052f3a08fb734164abd27c9dac0b841
wangxiaomo <wxm4ever@gmail.com>
2016-07-01 19:44:08 +0800

    add sass support

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var config = require("./config.json");
 var gulp = require("gulp"),
     cssmin = require("gulp-cssmin"),
     imagemin = require("gulp-imagemin"),
+    sass = require("gulp-sass"),
     filter = require("./filter.js"), // Hack gulp-filter using abs path
     del = require("del");
 
 gulp.task("clean", function() {
   del([config.build]);
+});
+
+gulp.task("sass", function() {
+  gulp.src(config.assets_directory + "**")
+    .pipe(filter("**/*.sass"))
+    .pipe(sass({outputStyle:config.sass_style}).on('error', sass.logError))
+    .pipe(gulp.dest(config.build));
 });
 
 gulp.task("cssmin", function() {
