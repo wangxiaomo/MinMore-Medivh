16d95e9bfd6009b65144251122b621c8451a1438
wangxiaomo <wxm4ever@gmail.com>
2016-07-04 10:01:05 +0800

    use pump and add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,24 @@ var gulp = require("gulp"),
     cssmin = require("gulp-cssmin"),
     imagemin = require("gulp-imagemin"),
     sass = require("gulp-sass"),
+    uglify = require("gulp-uglify"),
     filter = require("./filter.js"), // Hack gulp-filter using abs path
+    pump = require("pump"),
     del = require("del");
 
 gulp.task("clean", function() {
   del([config.build]);
 });
 
-gulp.task("sass", function() {
-  gulp.src(config.assets_directory + "**")
-    .pipe(filter("**/*.sass"))
-    .pipe(sass({outputStyle:config.sass_style}).on('error', sass.logError))
-    .pipe(gulp.dest(config.build));
+gulp.task("sass", function(cb) {
+  pump([
+      gulp.src(config.assets_directory + "**"),
+      filter("**/*.sass"),
+      sass({outputStyle:config.sass_style}),
+      gulp.dest(config.build)
+    ],
+    cb
+  );
 });
 
 gulp.task("cssmin", function() {
@@ -31,4 +37,15 @@ gulp.task("imagemin", function() {
     ]))
     .pipe(imagemin({verbose:config.verbose}))
     .pipe(gulp.dest(config.build));
+});
+
+gulp.task("js", function(cb) {
+  pump([
+      gulp.src(config.assets_directory + "**"),
+      filter("**/*.js"),
+      uglify(),
+      gulp.dest(config.build)
+    ],
+    cb
+  );
 });
