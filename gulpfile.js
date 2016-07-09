74bcee7246716b041129db9b291f3ed12e89353e
wangxiaomo <wxm4ever@gmail.com>
2016-07-09 09:15:13 +0800

    add compress flag

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var config = require("./config.json");
 var gulp = require("gulp"),
+    gulpif = require("gulp-if"),
     cleancss = require("gulp-clean-css"),
     cssmin = require("gulp-cssmin"),
     imagemin = require("gulp-imagemin"),
@@ -11,6 +12,9 @@ var gulp = require("gulp"),
     pump = require("pump"),
     del = require("del");
 
+config.sass_style = config.compress?"compressed":config.default_sass_style;
+config.clean_css = config.compress?{}:{"keepBreaks": true};
+
 gulp.task("clean", function() {
   del([config.build]);
 });
@@ -21,22 +25,23 @@ gulp.task("sass", function(cb) {
       filter("**/*.sass"),
       changed(config.build),
       sass({outputStyle:config.sass_style}),
+      cleancss(config.clean_css),
       gulp.dest(config.build)
     ],
     cb
   );
 });
 
-gulp.task("cssmin", function() {
+gulp.task("css", function() {
   gulp.src(config.assets_directory + "**")
     .pipe(filter("**/*.css"))
     .pipe(changed(config.build))
-    .pipe(cleancss())
-    .pipe(cssmin({showLog:config.verbose}))
+    .pipe(cleancss(config.clean_css))
+    .pipe(gulpif(config.compress, cssmin({showLog:config.verbose})))
     .pipe(gulp.dest(config.build));
 });
 
-gulp.task("imagemin", function() {
+gulp.task("image", function() {
   gulp.src(config.assets_directory + "**")
     .pipe(filter([
       "**/*.png", "**/*.jpg"
@@ -51,7 +56,7 @@ gulp.task("js", function(cb) {
       gulp.src(config.assets_directory + "**"),
       filter("**/*.js"),
       changed(config.build),
-      uglify(),
+      gulpif(config.compress, uglify()),
       gulp.dest(config.build)
     ],
     cb
@@ -63,7 +68,7 @@ gulp.task("watch", function() {
     gulp.start("sass");
   });
   watch(config.assets_directory + "**/*.css", {verbose:config.verbose}, function(){
-    gulp.start("cssmin");
+    gulp.start("css");
   });
   watch(config.assets_directory + "**/*.js", {verbose:config.verbose}, function(){
     gulp.start("js");
@@ -72,7 +77,7 @@ gulp.task("watch", function() {
       config.assets_directory + "**/*.png",
       config.assets_directory + "**/*.jpg",
     ], {verbose:config.verbose}, function(){
-      gulp.start("imagemin");
+      gulp.start("image");
     }
   );
 });
